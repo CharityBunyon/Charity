226281eba50da4aeec90abb9a92f692362cf2254
Charity Bunyon <cseymou5@gmail.com>
2020-02-21 11:32:26 -0600

    added project carousel

--- src/javascripts/components/navbar/navbar.js
+++ src/javascripts/components/navbar/navbar.js
@@ -9,11 +9,11 @@ const buildNav = () => {
   <a class="navbar-brand" href="#">
   <img src=${logo} class="navImage">
   </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+  <button class="navbar-toggler navTog" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon hamburger"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+  <div class="collapse navbar-collapse hamburger" id="navbarTogglerDemo02">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0 mainLinks container  justify-content-end">
       <li class="nav-item">
         <a class="nav-link" id="individualLink" href="#">About</a>
